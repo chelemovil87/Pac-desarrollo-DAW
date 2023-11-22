86a45945c337effa93218c1596c362d78b389e11
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2023-11-22 20:20:52 +0100

    codigo finalizado con comentarios

--- script2.js
+++ script2.js
@@ -1,68 +1,84 @@
-let numPlantas = document.getElementById("numPlantas").value;
-let plantas = Array.from({length: 6}, (_, i) => ({ id: "planta" + (i + 1), tamaño: 0 }));
+let numPlantas = document.getElementById("numPlantas").value; // Obtiene el valor del elemento HTML con el id "numPlantas"
+let plantas = Array.from({ length: 6 }, (_, i) => ({
+  // Crea un array de objetos, cada uno representando una planta
+  id: "planta" + (i + 1), // El id de la planta es una cadena que concatena "planta" con el índice de la planta en el array (más uno)
+  tamaño: 0, // El tamaño inicial de la planta es 0
+}));
 
-document.getElementById("iniciar").addEventListener("click", iniciarCrecimiento);
-document.getElementById("replantar").addEventListener("click", replantar);
+document
+  .getElementById("iniciar")
+  .addEventListener("click", iniciarCrecimiento); // Agrega un oyente de eventos al elemento con el id "iniciar"
+document.getElementById("replantar").addEventListener("click", replantar); // Agrega un oyente de eventos al elemento con el id "replantar"
 
 function iniciarCrecimiento() {
-    document.getElementById("iniciar").classList.add("oculto");
-    document.getElementById("replantar").classList.remove("oculto");
+  document.getElementById("iniciar").classList.add("oculto"); // Oculta el botón "iniciar"
+  document.getElementById("replantar").classList.remove("oculto"); // Muestra el botón "replantar"
 
-    numPlantas = document.getElementById("numPlantas").value;
-    let plantasSeleccionadas = plantas.slice(0, numPlantas);
+  numPlantas = document.getElementById("numPlantas").value; // Obtiene el número de plantas a crecer desde el input "numPlantas"
+  let plantasSeleccionadas = plantas.slice(0, numPlantas); // Selecciona las plantas a crecer
 
-    let alturaInvernadero = document.getElementById('invernadero').offsetHeight;
+  let alturaInvernadero = document.getElementById("invernadero").offsetHeight; // Obtiene la altura del invernadero
 
-    plantasSeleccionadas.forEach(planta => {
-        let alturaInicial = document.getElementById(planta.id).offsetHeight; 
-        let crecimiento = setInterval(() => {
-            let alturaPlanta = alturaInicial + (planta.tamaño / 100) * alturaInvernadero; 
-            if (alturaPlanta < alturaInvernadero) {
-                planta.tamaño += Math.floor(Math.random() * 20) + 1;
-                document.getElementById(planta.id).style.height = `${alturaPlanta}px`;
-            }
-            actualizarTarjetas();
-        }, 1000);
+  plantasSeleccionadas.forEach((planta) => {
+    // Para cada planta seleccionada
+    let alturaInicial = document.getElementById(planta.id).offsetHeight; // Obtiene la altura inicial de la planta
+    let crecimiento = setInterval(() => {
+      // Inicia un intervalo
+      let alturaPlanta =
+        alturaInicial + (planta.tamaño / 100) * alturaInvernadero; // Calcula la altura de la planta
+      if (alturaPlanta < alturaInvernadero) {
+        // Si la planta no ha alcanzado la altura máxima del invernadero
+        planta.tamaño += Math.floor(Math.random() * 20) + 1; // Aumenta el tamaño de la planta
+        document.getElementById(planta.id).style.height = `${alturaPlanta}px`; // Actualiza la altura de la planta en el HTML
+      }
+      actualizarTarjetas(); // Actualiza las tarjetas de las plantas
+    }, 1000); // El intervalo se ejecuta cada segundo
 
-        setTimeout(() => {
-            clearInterval(crecimiento);
-        }, 6000);
-    });
+    setTimeout(() => {
+      // Después de 6 segundos
+      clearInterval(crecimiento); // Detiene el intervalo
+    }, 6000);
+  });
 }
 
 function actualizarTarjetas() {
-    let cardContainer = document.getElementById('cardContainer');
-    cardContainer.innerHTML = '';
+  let cardContainer = document.getElementById("cardContainer"); // Obtiene el contenedor de las tarjetas
+  cardContainer.innerHTML = ""; // Limpia el contenedor de las tarjetas
 
-    let plantasSeleccionadas = plantas.slice(0, numPlantas);
+  let plantasSeleccionadas = plantas.slice(0, numPlantas); // Selecciona las plantas a mostrar
 
-    plantasSeleccionadas.sort((a, b) => b.tamaño - a.tamaño).forEach(planta => {
-        let card = document.createElement('div');
-        card.className = 'card';
-        card.innerHTML = `<img src="/sources/img/${planta.id}.png" alt="${planta.id}">
-                          <div>
-                            <p>${planta.tamaño}</p>
-                            <p>cm</p>
-                          </div>`;
-        cardContainer.appendChild(card);
+  plantasSeleccionadas
+    .sort((a, b) => b.tamaño - a.tamaño)
+    .forEach((planta) => {
+      // Para cada planta seleccionada, ordenadas por tamaño
+      let card = document.createElement("div"); // Crea una nueva tarjeta
+      card.className = "card"; // Asigna la clase "card" a la tarjeta
+      card.innerHTML = `<img src="/sources/img/${planta.id}.png" alt="${planta.id}"> // Agrega una imagen a la tarjeta
+                      <div>
+                        <p>${planta.tamaño}</p> // Agrega el tamaño de la planta a la tarjeta
+                        <p>cm</p> // Indica que el tamaño está en centímetros
+                      </div>`;
+      cardContainer.appendChild(card); // Agrega la tarjeta al contenedor
     });
 
-    cardContainer.style.visibility = 'visible';
+  cardContainer.style.visibility = "visible"; // Hace visible el contenedor de las tarjetas
 }
+
 function replantar() {
-document.getElementById("replantar").classList.add("oculto");
-document.getElementById("iniciar").classList.remove("oculto");
+  document.getElementById("replantar").classList.add("oculto"); // Oculta el botón "replantar"
+  document.getElementById("iniciar").classList.remove("oculto"); // Muestra el botón "iniciar"
 
-// Eliminar tarjetas de plantas
-var cardContainer = document.getElementById('cardContainer');
-    cardContainer.style.visibility = 'hidden';
-    cardContainer.innerHTML = '';
+  // Elimina las tarjetas de las plantas
+  var cardContainer = document.getElementById("cardContainer");
+  cardContainer.style.visibility = "hidden"; // Oculta el contenedor de las tarjetas
+  cardContainer.innerHTML = ""; // Limpia el contenedor de las tarjetas
 
-plantas.forEach(planta => {
-    planta.tamaño = 0;
-       document.getElementById(planta.id).style.height = "auto"; 
-    });
+  plantas.forEach((planta) => {
+    // Para cada planta
+    planta.tamaño = 0; // Restablece el tamaño de la planta a 0
+    document.getElementById(planta.id).style.height = "auto"; // Restablece la altura de la planta en el HTML a su valor por defecto
+  });
 }
 
-var sound = new Audio();
-sound.src = "/sources/sound/sonido.mp3";
+var sound = new Audio(); // Crea un nuevo objeto Audio
+sound.src = "/sources/sound/sonido.mp3"; // Asigna la fuente del sonido al archivo "sonido.mp3"
